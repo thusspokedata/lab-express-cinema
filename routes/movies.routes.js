3cb549fa425259c5a7b70dc37c9691f91836bffb
Antonio Saleme <79271990+thusspokedata@users.noreply.github.com>
2022-11-15 00:16:43 +0100

    done

--- routes/movies.routes.js
+++ routes/movies.routes.js
@@ -13,4 +13,27 @@ router.get('/movies', (req, res) => {
     });
 });
 
+router.get('/movie/:id', async (req, res) => {
+  const id = req.params.id;
+  try {
+    const info = await Movie.findById(id);
+    res.render('movies/movie-details', info);
+  } catch (err) {
+    console.log(err);
+    res.redirect('/movies');
+  }
+});
+
+// router.get('/movie/:id', (req, res) => {
+//   const id = req.params.id;
+//   Movie.findById(id)
+//     .then((data) => {
+//       res.render('movies/movie-details', data);
+//     })
+//     .catch((err) => {
+//       console.log(err);
+//       res.redirect('/movies');
+//     });
+// });
+
 module.exports = router;
